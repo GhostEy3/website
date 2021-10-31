365755b8e1c65b01bca7f237c022856d146fa073
GhostEy3 <saidobid.rasulson@gmail.com>
2021-10-31 14:41:09 +0100

    6 commit

--- mail.php
+++ mail.php
@@ -28,7 +28,7 @@ if ($name == "") {
     $msg['field'] = "contact-message";
     $msg['code'] = FALSE;
 } else {
-    $to = 'contact@example.com';
+    $to = 'saidobid@rasulson.com';
     $subject = 'Papr Contact Query';
     $_message = '<html><head></head><body>';
     $_message .= '<p>Name: ' . $name . '</p>';
@@ -39,10 +39,10 @@ if ($name == "") {
 
     $headers = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-    $headers .= 'From:  Papr <contact@example.com>' . "\r\n";
-    $headers .= 'cc: contact@example.com' . "\r\n";
-    $headers .= 'bcc: contact@example.com' . "\r\n";
-    mail($to, $subject, $_message, $headers, '-f contact@example.com');
+    $headers .= 'From:  Papr <saidobid@rasulson.com>' . "\r\n";
+    $headers .= 'cc: saidobid@rasulson.com' . "\r\n";
+    $headers .= 'bcc: saidobid@rasulson.com' . "\r\n";
+    mail($to, $subject, $_message, $headers, '-f saidobid@rasulson.com');
 
     $msg['success'] = "\n Email has been sent successfully.";
     $msg['code'] = TRUE;
